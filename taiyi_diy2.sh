398c16a0894d9263e2a5becec161fde87ee17425
MIT-X <134132915+MIT-X@users.noreply.github.com>
2025-04-04 09:25:05 +0800

    Update taiyi_diy2.sh

--- taiyi_diy2.sh
+++ taiyi_diy2.sh
@@ -2,4 +2,4 @@
 # ============================================================================================
 
 # 1-修改内核版本
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' .target/linux/qualcommax/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/qualcommax/Makefile
