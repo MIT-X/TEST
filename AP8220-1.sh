e831bf7078ea4f1d63b6f1ea1c0f17f70a6c600f
MIT-X <134132915+MIT-X@users.noreply.github.com>
2026-05-26 09:53:58 +0800

    Delete AP8220-1.sh

--- AP8220-1.sh
+++
@@ -1,9 +0,0 @@
-# 此脚本用处是：添加第三方插件
-#=========================================================================================================================
-
-# 1-添加 集客 插件
-# git clone https://github.com/MIT-X/luci-app-gecoosac.git package/luci-app-gecoosac
-
-# 2-添加 PassWall2 插件
-echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main" >> "feeds.conf.default"
-echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
