7b0254cc4f058b45d7265b49f036d37d768b118e
MIT-X <134132915+MIT-X@users.noreply.github.com>
2026-05-26 15:47:30 +0800

    Update Scripts.sh

--- Scripts.sh
+++ Scripts.sh
@@ -2,7 +2,7 @@
 #=========================================================================================================================
 
 # 1-添加 集客 插件
-git clone https://github.com/MIT-X/luci-app-gecoosac.git package/luci-app-gecoosac
+# git clone https://github.com/MIT-X/luci-app-gecoosac.git package/luci-app-gecoosac
 
 # 2-添加 PassWall2 插件
 echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main" >> "feeds.conf.default"
