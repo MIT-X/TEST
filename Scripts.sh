140d475a034aa2a2805e08a9a12f3903df6551ae
MIT-X <134132915+MIT-X@users.noreply.github.com>
2026-05-27 22:38:02 +0800

    Update Scripts.sh

--- Scripts.sh
+++ Scripts.sh
@@ -7,8 +7,6 @@ git clone https://github.com/MIT-X/luci-app-gecoosac.git package/luci-app-gecoos
 # 2-添加 关机 插件
 git clone https://github.com/MIT-X/luci-app-poweroff.git package/luci-app-poweroff
 
-# git clone https://github.com/laipeng668/luci-app-gecoosac.git package/luci-app-gecoosac
-
 # 3-添加 PassWall2 插件
 echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
